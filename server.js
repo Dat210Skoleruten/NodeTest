1b67f5f6475f6de6c5b471ce054ef20d9446914a
RabaGhast <fabianboe@hotmail.com>
2017-03-13 12:24:54 +0100

    changed get

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ var express = require('express');
 var app = express();
 
 app.get('/data', function(req, res){
-  res.sendFile(path.join(__dirname + '/index.html'));
-  //res.send('hello world with data'); //replace with your data here
+  //res.sendFile(path.join(__dirname + '/index.html'));
+  res.send('hello world with data'); //replace with your data here
 });
 
 app.listen(process.env.PORT || 3000);
