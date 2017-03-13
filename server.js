d8a97fc5e3051820c61df5f715049e1b26f8d1eb
thrasmussen <thrasmussen@outlook.com>
2017-03-13 12:47:41 +0100

    hello world test

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
-var express = require('express');
-var app = express();
-console.log("I am running so hot right now");
-app.get('/data', function(req, res){
-  //res.sendFile(path.join(__dirname + '/index.html'));
-  res.send('hello world with data'); //replace with your data here
-});
+// var express = require('express');
+// var app = express();
+// console.log("I am running so hot right now");
+// app.get('/data', function(req, res){
+//   //res.sendFile(path.join(__dirname + '/index.html'));
+//   res.send('hello world with data'); //replace with your data here
+// });
 
-app.listen(process.env.PORT || 3000);
+// app.listen(process.env.PORT || 3000);
+
+
+ var http = require('http')
+ var port = process.env.PORT || 1337;
+ http.createServer(function(req, res) {
+   res.writeHead(200, { 'Content-Type': 'text/plain' });
+   res.end('Hello World\n');
+ }).listen(port);
